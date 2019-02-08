8f0786d36764de56c0bc1404848c7ab8ec3f10ac
Danil <dchemaev03@mail.ru>
2019-02-08 15:56:53 +0400

    Движение персонажа

--- RPG.py
+++ RPG.py
@@ -4,10 +4,12 @@ import pygame
 import sys
 
 pygame.init()
+pygame.key.set_repeat(1, 10)
 
 FPS = 60
-WIDTH = 800
-HEIGHT = 700
+STEP = 7
+WIDTH = 600
+HEIGHT = 600
 TILE_WIDTH = TILE_HEIGHT = 50
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,6 +18,7 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()  # Группа всех спрайтов
 map_group = pygame.sprite.Group()  # Спрайты карты
 player_group = pygame.sprite.Group()  # Спрайты персонажей
+granny_group = pygame.sprite.Group()
 
 
 def load_image(name, color_key=None):
@@ -25,7 +28,7 @@ def load_image(name, color_key=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-    image = image.convert_alpha()
+    # image = image.convert_alpha()
 
     if color_key is not None:
         if color_key == -1:
@@ -40,47 +43,6 @@ tile_images = {
     'player': load_image('hero.jpg'),
     # 'badguy': load_image('mario.png')
 }
-
-
-def generate_level(level):
-    new_player = None
-    main_granny = None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '$':
-                Tile('granny', x, y)
-                main_granny = Granny(x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = MainHero(x, y)
-
-    return new_player, main_granny
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(map_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x,
-                                               TILE_HEIGHT * pos_y)
-
-
-def load_level(filename):
-    filename = 'data/' + filename
-
-    with open(filename, 'r') as map_file:
-        level_map = [line.strip() for line in map_file]
-
-    max_width = max(map(len, level_map))
-
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
 
 
 def start_screen():
@@ -113,30 +75,127 @@ def start_screen():
         clock.tick(FPS)
 
 
-class MainHero(pygame.sprite.Sprite):
-    pass
+def generate_level(level):
+    new_player = None
+    main_granny = None
+    x, y = None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.' or level[y][x] == "#":
+                Tile('empty', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = MainHero(x, y)
+            elif level[y][x] == '$':
+                Tile('empty', x, y)
+                main_granny = Granny(x, y)
+
+    return new_player, main_granny, x, y
 
 
 class Granny(pygame.sprite.Sprite):
-    pass
+    def __init__(self, pos_x, pos_y):
+        super().__init__(granny_group, all_sprites)
+        self.image = tile_images["granny"]
+        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x,
+                                               TILE_HEIGHT * pos_y)
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(map_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x,
+                                               TILE_HEIGHT * pos_y)
+
+
+def load_level(filename):
+    filename = 'data/' + filename
+
+    with open(filename, 'r') as map_file:
+        level_map = [line.strip() for line in map_file]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class MainHero(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = tile_images['player']
+        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x + 15,
+                                               TILE_HEIGHT * pos_y + 5)
+
+
+class Camera:
+    # зададим начальный сдвиг камеры и размер поля для возможности реализации циклического сдвига
+    def __init__(self, field_size):
+        self.dx = 0
+        self.dy = 0
+        self.field_size = field_size
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        # вычислим координату клитки, если она уехала влево за границу экрана
+        if obj.rect.x < -obj.rect.width:
+            obj.rect.x += (self.field_size[0] + 1) * obj.rect.width
+        # вычислим координату клитки, если она уехала вправо за границу экрана
+        if obj.rect.x >= (self.field_size[0]) * obj.rect.width:
+            obj.rect.x += -obj.rect.width * (1 + self.field_size[0])
+        obj.rect.y += self.dy
+        # вычислим координату клитки, если она уехала вверх за границу экрана
+        if obj.rect.y < -obj.rect.height:
+            obj.rect.y += (self.field_size[1] + 1) * obj.rect.height
+        # вычислим координату клитки, если она уехала вниз за границу экрана
+        if obj.rect.y >= (self.field_size[1]) * obj.rect.height:
+            obj.rect.y += -obj.rect.height * (1 + self.field_size[1])
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
 
 start_screen()
+level = load_level('levelex.txt')
+player, granny, level_x, level_y = generate_level(level)
+camera = Camera((level_x, level_y))
+
+"""""
+if Granny(x, y) == MainHero(x, y):
+    terminate()
+"""""
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.rect.x -= STEP
+            if event.key == pygame.K_RIGHT:
+                player.rect.x += STEP
+            if event.key == pygame.K_UP:
+                player.rect.y -= STEP
+            if event.key == pygame.K_DOWN:
+                player.rect.y += STEP
+    camera.update(player)
     all_sprites.update()
     screen.fill(pygame.Color("white"))
-    all_sprites.draw(screen)
+    map_group.draw(screen)
+    granny_group.draw(screen)
+    player_group.draw(screen)
 
     pygame.display.flip()
     clock.tick(FPS)
-    map_group.draw(screen)
-    player_group.draw(screen)
 
     pygame.display.flip()
     clock.tick(FPS)
